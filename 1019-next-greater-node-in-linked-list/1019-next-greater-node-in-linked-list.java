955e3fdaee3b7107f988e0f4c502d4ad93dac00a
aarushichaddha <168860140+aarushichaddha@users.noreply.github.com>
2026-08-06 22:51:28 +0530

    Time: 590 ms (17.23%), Space: 50 MB (35.95%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -1,29 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-
+        
         ArrayList<Integer> list = new ArrayList<>();
 
         ListNode curr = head;
-
-        while (curr != null) {
-
-            ListNode temp = curr.next;
+        while(curr!=null){
+            ListNode tmp = curr.next;
             int ans = 0;
 
-            while (temp != null) {
-                if (temp.val > curr.val) {
-                    ans = temp.val;
+            while(tmp!=null){
+                if(curr.val<tmp.val){
+                    ans = tmp.val;
                     break;
                 }
-                temp = temp.next;
+                tmp = tmp.next;
+
             }
 
             list.add(ans);
             curr = curr.next;
         }
 
-        int[] arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        int arr[] = new int[list.size()];
+
+        for(int i=0;i<list.size();i++){
             arr[i] = list.get(i);
         }
 
